9985230d9a1815a0575c1a690c6628d149d8d214
Roi <68617740+Roizor@users.noreply.github.com>
2023-03-02 18:18:19 -0600

    default cfg

--- src/sounds.js
+++ src/sounds.js
@@ -1,4 +1,4 @@
-const SoundOnPress = true
-const ScreenSaverActivationTime = 5
-const Sounds = [{"name":"Shooting","keys":["alt","f24"],"icon":"shooting.png"},{"name":"Footsteps","key":"f23","icon":"footsteps.png"},{"name":"Whopper","key":"f22","icon":"whopper.png"},{"name":"Didn't I Do It","key":"f18","icon":"borzio.png"},{"name":"Biggest Bird","key":"f21","icon":"bird.png"},{"name":"Disconnect","key":"f20","icon":"disconnect.png"},{"name":"Vine Boom","key":"f16","icon":"boom.png"},{"name":"Semtex","key":"f17","icon":"semtex.png"},{"name":"Alt Tab","keys":["alt","tab"],"icon":"alt_tab.png"},{"name":"Ohio Sound","keys":["alt","f23"],"icon":"ohio.png"}]
+const SoundOnPress = false
+const ScreenSaverActivationTime = 60
+const Sounds = [{"name":"Shooting","keys":["alt","f24"],"icon":"shooting.png"},{"name":"Footsteps","key":"f23","icon":"footsteps.png"},{"name":"Whopper","key":"f22","icon":"whopper.png"},{"name":"Didn't I Do It","key":"f18","icon":"borzio.png"},{"name":"Biggest Bird","key":"f21","icon":"bird.png"},{"name":"Disconnect","key":"f20","icon":"disconnect.png"},{"name":"Vine Boom","key":"f16","icon":"boom.png"},{"name":"Semtex","key":"f17","icon":"semtex.png"},{"name":"Alt Tab","keys":["alt","tab"],"icon":"alt_tab.png"},{"name":"Ohio Sound","keys":["alt","f23"],"icon":"ohio.png"},{"name":"New Key","key":"enter"}]
 module.exports = { SoundOnPress, ScreenSaverActivationTime, Sounds }

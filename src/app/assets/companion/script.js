79b126953aff79fc6efc116c8b286a6bcc13a60c
Roi <68617740+Roizor@users.noreply.github.com>
2023-03-06 19:14:05 -0600

    add some things

--- src/app/assets/companion/script.js
+++ src/app/assets/companion/script.js
@@ -192,3 +192,14 @@ function bp () {
     loadPage(currentPage - 1)
   }
 }
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+} 

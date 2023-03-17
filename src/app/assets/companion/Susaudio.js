2f91717d6fff1fcdb926dd72727de324da18d1f8
Roi <68617740+Roizor@users.noreply.github.com>
2023-03-17 00:02:43 -0500

    new audio funcs

--- src/app/assets/companion/Susaudio.js
+++ src/app/assets/companion/Susaudio.js
@@ -43,6 +43,25 @@ const Susaudio = {
   stopRecent: () => {
     Susaudio._player.queue[0].stop()
     Susaudio._player.queue = _sa_removeFromArray(Susaudio._player.queue, Susaudio._player.queue[0])
+  },
+  playPause: () => {
+    Susaudio._player.queue.forEach(audio => {
+      if (paused == true) {
+        paused = false
+        audio.play()
+      } else {
+        paused = true
+        audio.pause()
+      }
+    })
+  },
+  changeAllPlayingPitch: (num) => {
+    Susaudio._player.queue.forEach(audio => {
+      audio.pause()
+      audio.preservesPitch = false
+      audio.playbackRate = num
+      audio.play()
+    })
   }
 }
 

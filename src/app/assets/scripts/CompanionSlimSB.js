ba3af153958a0758bcd5fd94f6312eefdf1c9756
Roi <68617740+Roizor@users.noreply.github.com>
2023-04-06 18:11:10 -0500

    fix now playing

--- src/app/assets/scripts/CompanionSlimSB.js
+++ src/app/assets/scripts/CompanionSlimSB.js
@@ -1,3 +1,5 @@
+const soundQueue = []
+
 /* eslint-disable no-undef */
 socket.on('press-sound', (sound, name) => {
   if (sound.includes('--Stop_all')) {
@@ -9,3 +11,16 @@ socket.on('press-sound', (sound, name) => {
   Susaudio.playSound(sound, name, true)
   susdeckUniversal.socket.emit('c2s_log', '[COMPANIONSB] Playing ' + name)
 })
+
+if (typeof document.getElementById('now-playing') !== 'undefined') {
+  setInterval(() => {
+    soundQueue.splice(0, soundQueue.length)
+    // eslint-disable-next-line no-undef
+    Susaudio._player.queue.forEach(audio => {
+      if (audio.isSusaudioFB) return
+      if (!audio.isNotVB) return
+      soundQueue.push(audio.saName)
+    })
+    document.getElementById('now-playing').innerText = 'Playing: ' + soundQueue.join(', ')
+  }, 250)
+}

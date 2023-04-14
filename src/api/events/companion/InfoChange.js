7890017e522b31c74715c91721d5711c82d1c4b9
Roi <68617740+Roizor@users.noreply.github.com>
2023-04-14 00:47:23 -0500

    change some thinsg

--- src/api/events/companion/InfoChange.js
+++ src/api/events/companion/InfoChange.js
@@ -22,20 +22,25 @@ module.exports = {
         }
       })
       Object.assign(found, newObject)
+
       fs.writeFileSync('./src/settings/sounds.js', `const SoundOnPress = ${sounds.SoundOnPress}
 const ScreenSaverActivationTime = ${sounds.ScreenSaverActivationTime}
 const Sounds = ${jsonbeautify(sounds.Sounds)}
-if (typeof module !== 'undefined') module.exports = { SoundOnPress, ScreenSaverActivationTime, Sounds }`)
+if (typeof module !== 'undefined') module.exports = { SoundOnPress, ScreenSaverActivationTime, Sounds }
+`)
+
       return 'c-change'
     } else if (args.includes(',')) {
       args = args.split(',')
       const newssattime = args[0].split('SSAT:')[1]
       const newssoc = args[1].split('SOC:')[1]
+
       fs.writeFileSync('./src/settings/sounds.js', `const SoundOnPress = ${newssoc}
 const ScreenSaverActivationTime = ${newssattime}
 const Sounds = ${jsonbeautify(sounds.Sounds, null, 4, 80)}
 if (typeof module !== 'undefined') module.exports = { SoundOnPress, ScreenSaverActivationTime, Sounds }
 `)
+
       console.log(`const SoundOnPress = ${newssattime}`)
     }
   }

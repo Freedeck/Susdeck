ad9f0f1fe3c2bf3c055e9d0704a353b0e63e0742
Roizor <aidenjs9@icloud.com>
2023-04-16 21:28:22 -0500

    this removes clutter

--- src/app/assets/scripts/loader.js
+++ src/app/assets/scripts/loader.js
@@ -127,3 +127,31 @@ window.addEventListener('load', () => {
 
 // Server has changed something, reload over client
 socket.on('c-change', () => { window.location.replace(window.location.href) })
+
+/* eslint-disable no-undef */
+let touchstartX = 0
+let touchendX = 2500
+
+function checkDirection () {
+  if (touchendX < touchstartX) {
+    // go page up
+    if (Pages[currentPage + 1]) {
+      loadPage(currentPage + 1)
+    } else { /* empty */ }
+  }
+  if (touchendX > touchstartX) {
+    // go page down
+    if (Pages[currentPage - 1]) {
+      loadPage(currentPage - 1)
+    } else { /* empty */ }
+  }
+}
+
+document.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+})
+
+document.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX
+  checkDirection()
+})

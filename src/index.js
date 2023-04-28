f16eba95af1f6396fb1a0cba945ca11acf06c9d0
Roi <68617740+Roizor@users.noreply.github.com>
2023-04-27 23:48:20 -0500

    wrap it in a try cathc

--- src/index.js
+++ src/index.js
@@ -12,16 +12,20 @@ const sockApiInit = require('./api/init').init;
 
 const port = settings.Port || process.env.PORT || 5754;
 
-sockApiInit(io, app); // Socket API initialize
+try {
+  sockApiInit(io, app); // Socket API initialize
 
-app.use('/', ex.static('./src/app')); // Initialize HTTP routes for main web app directory
+  app.use('/', ex.static('./src/app')); // Initialize HTTP routes for main web app directory
 
-app.get('/sounds.js', (req, res) => { res.sendFile(path.join(__dirname, '/settings/sounds.js')); }); // Make sounds.js accessible from apps
+  app.get('/sounds.js', (req, res) => { res.sendFile(path.join(__dirname, '/settings/sounds.js')); }); // Make sounds.js accessible from apps
 
-httpServer.listen(port, () => {
-  console.log('Susdeck v' + require('../package.json').version + ' Web Host is starting - starting Companion');
-  require('child_process').exec('npx electron src/companion'); // Start Companion on another process
-  Object.keys(getNetworkInterfaces()).forEach(netInterface => {
-    console.log('Go to ' + getNetworkInterfaces()[netInterface][0] + ':' + port + ' on your mobile device [Interface ' + netInterface + ']');
+  httpServer.listen(port, () => {
+    console.log('Susdeck v' + require('../package.json').version + ' Web Host is starting - starting Companion');
+    require('child_process').exec('npx electron src/companion'); // Start Companion on another process
+    Object.keys(getNetworkInterfaces()).forEach(netInterface => {
+      console.log('Go to ' + getNetworkInterfaces()[netInterface][0] + ':' + port + ' on your mobile device [Interface ' + netInterface + ']');
+    });
   });
-});
+} catch (err) {
+  console.log('Presumably fatal error:',err);
+}

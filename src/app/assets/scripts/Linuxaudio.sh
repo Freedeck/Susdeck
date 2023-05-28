9696bd649c10b21949971b53b969d1c9c832b813
Roizor <aidenjs9@icloud.com>
2023-05-27 21:54:09 -0500

    clean uip a bit

--- src/app/assets/scripts/Linuxaudio.sh
+++
@@ -1,9 +0,0 @@
-export LC_ALL=C
-DEFAULT_OUTPUT=$(pactl info|sed -n -e 's/^.*Default Sink: //p')
-if ! pactl info|grep -w "PipeWire">/dev/null; then
-    pactl unload-module module-default-device-restore
-fi
-pactl load-module module-null-sink sink_name=susdeckin
-pactl load-module module-null-sink sink_name=susdeckout
-pactl load-module module-loopback source=susdeckout.monitor sink=susdeckin.monitor
-pactl load-module module-remap-source master=susdeckout.monitor source_name=Susdeck source_properties=device.description=Susdeck

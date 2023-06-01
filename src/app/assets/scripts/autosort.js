c0520497a13092c90c9a757bdc8e33ff0bc8a5b4
Roizor <aidenjs9@icloud.com>
2023-05-31 21:23:07 -0500

    there we go, now it fully sorts correcty

--- src/app/assets/scripts/autosort.js
+++ src/app/assets/scripts/autosort.js
@@ -7,21 +7,8 @@ let pagesAmount = Sounds.length / countOnEachPage;
 let pageCounter = 0;
 let index = 0;
 
-for (let i = 0; i < pagesAmount; i++) {
-  Pages[i] = []; // Loop through and clear each page
-}
-
-Sounds.forEach(sound => { // Loop through each sound
-  Pages[pageCounter].push(sound); // Add it to a page
-  if (index === countOnEachPage) { // However, if we reach the max icon count for the screen,
-    pageCounter++; // Increment the page
-    index = 0; // And reset the sound amount counter
-  }
-  index++; // Increment sound index/amount
-});
-
 function autosort (countOnEP) {
-  pagesAmount = Sounds.length / countOnEP; // Set the amount of pages
+  pagesAmount = Math.floor(Sounds.length / countOnEP); // Set the amount of pages
   for (let i = 0; i < pagesAmount; i++) {
     Pages[i] = []; // Loop through and clear each page
   }
@@ -34,6 +21,7 @@ function autosort (countOnEP) {
     if (index === countOnEP) { // However, if we reach the max icon count for the screen,
       pageCounter++; // Increment the page
       index = 0; // And reset the sound amount counter
+      return;
     }
     index++; // Increment sound index/amount
   });

570eb2c12c3eb0b15595f0f46cde5db296bce46d
Roizor <aidenjs9@icloud.com>
2023-07-18 16:36:32 -0500

    asdfghj

--- src/settings/sounds.js
+++ src/settings/sounds.js
@@ -2,5 +2,5 @@
 const SoundOnPress = false;
 const ScreenSaverActivationTime = 5;
 const soundDir = '../assets/sounds/';
-const Sounds = [{"name":"RAHH","path":"rah.mp3"}, {"name": "Open Companion to edit this!", "path":"Edit me!"}];
+const Sounds = [{"name":"Shooting","icon":"shooting.png","path":"shooting.mp3"},{"name":"Footsteps","icon":"footsteps.png","path":"loudfootsteps.mp3"},{"name":"Whoppah","path":"WHOPPER.mp3","icon":"whopper.png"},{"name":"Didn't I Do It","icon":"borzoi.png","path":"borzio.mp3"},{"name":"Biggest Bird","icon":"bird.png","path":"biggestbird.wav"},{"name":"Disconnect","icon":"disconnect.png","path":"disconnect.mp3"},{"name":"Vine Boom","icon":"boom.png","path":"vineboom.mp3"},{"name":"Semtex","icon":"semtex.png","path":"semtex.mp3"},{"name":"Huh","path":"huh.mp3"},{"name":"Haha","path":"haha.mp3"},{"name":"Alt Tab","keys":"[\"alt\",\"tab\"]"},{"name":"Whoppah Remix","path":"wopha_remix.wav"},{"name":"Bugatti","path":"bugatti.mp3"},{"name":"Metal Pipe","path":"metal_pipe.mp3"},{"name":"RAHH","path":"rah.mp3"}];
 if (typeof module !== 'undefined') module.exports = { SoundOnPress, ScreenSaverActivationTime, soundDir, Sounds };

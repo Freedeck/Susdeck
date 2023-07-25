ec6487a98ec8d7323177b99654c658ba76312941
Roizor <aidenjs9@icloud.com>
2023-07-24 21:18:23 -0500

    Edit start message, add version checking

--- src/index.js
+++ src/index.js
@@ -9,12 +9,23 @@ const io = require('socket.io')(httpServer);
 
 const settings = require('../Settings');
 const getNetworkInterfaces = require('./util/network');
+const sounds = require('./settings/sounds');
+const fd = require('../package.json');
 const sockApiInit = require('./api/init').init;
 
 const port = settings.Port || process.env.PORT || 5754;
 
 try {
+  console.clear();
+  console.log('Freedeck v' + fd.version);
   debug.log('Version matching');
+  // Do config versions match the hosts?
+  if (settings.fdv !== fd.version) {
+    console.log('[IMPORTANT!]\nYour settings config is out of date. Rerun npm run setup to update it!');
+  }
+  if(sounds.cfg.v !== fd.version) {
+    console.log('[IMPORTANT!]\nYour sound config is out of date. If Freedeck doesn\'t load, rerun npm run setup.\nIf Freedeck functions, use companion to:\n1: Edit any sound from the Icon Editor\n2:Press Set\n3:Update will automagically complete.');
+  }
   debug.log('Initializing Socket API');
   sockApiInit(io, app); // Socket API initialize
 
@@ -24,12 +35,15 @@ try {
   app.get('/sounds.js', (req, res) => { res.sendFile(path.join(__dirname, '/settings/sounds.js')); }); // Make sounds.js accessible from apps
 
   debug.log('Listening for ' + port);
-  if (process.argv[2] === '--no-server') require('child_process').exec('npx electron src/companion'); // Start Companion on another process
+  if (process.argv[2] === '--no-server') {
+    console.log('Companion is starting in serverless mode!');
+    require('child_process').exec('npx electron src/companion'); // Start Companion on another process
+  }
   if (process.argv[2] !== '--no-server') httpServer.listen(port, () => {
-    console.log('Freedeck v' + require('../package.json').version + ' Web Host is starting - starting Companion');
+    console.log('Web Host is starting - launching Companion');
     if (process.argv[2] !== '--headless') require('child_process').exec('npx electron src/companion'); // Start Companion on another process
     Object.keys(getNetworkInterfaces()).forEach(netInterface => {
-      console.log('Go to ' + getNetworkInterfaces()[netInterface][0] + ':' + port + ' on your mobile device [Interface ' + netInterface + ']');
+      console.log('Go to ' + getNetworkInterfaces()[netInterface][0] + ':' + port + ' on your mobile device [Interface ' + netInterface + ']')
     });
   });
 } catch (err) {

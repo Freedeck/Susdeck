289c987208c1298a54a861d6c258c3c01e2df827
Roizor <aidenjs9@icloud.com>
2023-10-15 09:02:13 -0500

    Use UUID instead of name (I thought I did this)

--- src/api/events/companion/DelKey.js
+++ src/api/events/companion/DelKey.js
@@ -7,7 +7,7 @@ const ev = new Event('fd.companion.delete-key', ({ socket, args, meta }) => {
     socket.emit('server_notification', 'You must have at least 1 button!');
     return false;
   }
-  sounds.Sounds.splice(sounds.Sounds.indexOf({ name: args.name }), 1);
+  sounds.Sounds.splice(sounds.Sounds.indexOf({ uuid: args.uuid }), 1);
   fs.writeFileSync('./src/settings/sounds.js', `/* eslint-disable quotes, quote-props, indent, no-unused-vars */
 const ScreenSaverActivationTime = ${sounds.ScreenSaverActivationTime};
 const soundDir = '../assets/sounds/';

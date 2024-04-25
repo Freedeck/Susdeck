eb9125cc763e5fb804c5b4cbaa109072f9f31501
Aiden <aidenjs9@icloud.com>
2024-04-25 18:32:18 -0500

    Autoupdater should work

--- src/entryChecker.js
+++ src/entryChecker.js
@@ -1,33 +1,30 @@
 const path = require('path');
-const package = require(path.resolve('./package.json'));
+const pkg = require(path.resolve('./package.json'));
 
 console.log('Checking for updates... (start with --no-update to skip)');
 if (process.argv.includes('--no-update')) {
   console.log('Skipping update check.');
 } else {
-  fetch("https://raw.githubusercontent.com/Freedeck/Freedeck/master/autoupdate")
-  .then((res)=>res.text())
-  .then((ver) => {
-	if(package.version !== ver) {
-		console.log('Update available! Installing update.'); 
-		const { spawn } = require('child_process');
+  fetch('https://raw.githubusercontent.com/Freedeck/Freedeck/master/autoupdate')
+      .then((res)=>res.text())
+      .then((ver) => {
+        if (pkg.version !== ver) {
+          console.log('Update available! Running \'git pull\'. [1/2]');
+          const {spawn} = require('child_process');
 
-// Create a new child process for 'git pull'
-const pull = spawn('git', ['pull'], { detached: true, stdio: 'ignore' });
-
-// Allow the parent to exit independently of the child
-pull.unref();
-
-// Create a new child process for 'git fetch'
-const fetch = spawn('git', ['fetch'], { detached: true, stdio: 'ignore' });
-
-// Allow the parent to exit independently of the child
-fetch.unref();
-
-// Exit the parent process
-process.exit();
-	}
-  })
+          // Create a new child process for 'git pull'
+          const spawned = spawn('git', ['pull'], {detached: true, stdio: 'ignore'});
+          spawned.on('exit', () => {
+            console.log('Pulled latest changes. Running \'git fetch\'. [2/2]');
+            const spawned2 = spawn('git', ['fetch'], {detached: true, stdio: 'ignore'});
+            spawned2.on('exit', () => {
+              console.log('Fetched latest changes. Please start Freedeck again.');
+			  process.exit(0);
+            });
+          });
+        } else {
+          require(path.resolve('./src/index.js'));
+        }
+      });
 }
 
-require(path.resolve('./src/index.js'));

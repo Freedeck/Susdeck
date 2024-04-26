75df53f391a002b4cbf58d2b2a5f38a6ede14e6d
Aiden <aidenjs9@icloud.com>
2024-04-26 13:40:02 -0500

    - add some cool stuff to monitor disabling

--- src/managers/plugins.js
+++ src/managers/plugins.js
@@ -6,6 +6,7 @@ const AsarBundleRunner = require('asar-bundle-runner');
 
 const pl = {
   _plc: new Map(),
+  _disabled: [],
   _tyc: new Map(),
   _ch: new Map(),
   plugins: () => {
@@ -31,8 +32,15 @@ const pl = {
     pl.update();
   },
   update: () => {
+    pl._disabled = [];
+    pl._plc.clear();
+    pl._tyc.clear();
     fs.readdirSync(path.resolve('./plugins')).forEach((file) => {
-      if (!file.endsWith('.Freedeck') || file.endsWith('.disabled')) return;
+      if (file.endsWith('.disabled')) {
+        pl._disabled.push(file);
+        return;
+      }
+      if (!file.endsWith('.Freedeck')) return;
       AsarBundleRunner.extract('./plugins/' + file).then((a) => {
         AsarBundleRunner.run(a).then((instantiated) => {
           debug.log(picocolors.yellow('Plugin initialized ' + instantiated.name + ' - ID ' + instantiated.id), 'Plugin Manager');

b62c7be7c717d7ec87f7b2de775855528c394d7a
Aiden <aidenjs9@icloud.com>
2024-04-27 12:54:29 -0500

    Stash changes in case something happens for whatever reason. Important, so version bumped.

--- src/entryChecker.js
+++ src/entryChecker.js
@@ -12,21 +12,19 @@ const autoupd = async () => {
 const ver = await autoDat.text()
 if (pkg.version !== ver) {
 	console.log('Update available! Running \'git pull\'. [1/2] (Updating from ' + pkg.version + ' to ' + ver + ')');
-	const {spawn} = require('child_process');
+	const {exec} = require('child_process');
 
 	// Create a new child process for 'git pull'
-	const spawned = spawn('git', ['pull'], {
-		cwd: path.resolve('./'),
-		detached: true, stdio: 'ignore'
-	});
-	spawned.on('exit', () => {
-	  console.log('Pulled latest changes. Running \'git fetch\'. [2/2]');
-	  const spawned2 = spawn('git', ['fetch'], {detached: true, stdio: 'ignore'});
-	  spawned2.on('exit', () => {
-		console.log('Fetched latest changes. Please start Freedeck again.');
-		process.exit(0);
-	  });
-	});
+	exec('git stash', () => {
+		console.log('Stashed just in case. Running \'git pull\'. [1/2]');
+		exec('git pull', () => {
+			console.log('Pulled latest changes. Running \'git fetch\'. [2/2]');
+			exec('git fetch', () => {
+			  console.log('Fetched latest changes. Please start Freedeck again.');
+			  process.exit(0);
+			});
+		});
+	})
   }
 }
 

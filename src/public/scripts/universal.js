98135a08fefc2567592aba85097b2a331eb146ac
Aiden <aidenjs9@icloud.com>
2024-04-27 12:43:03 -0500

    finally, import old themes!!!

--- src/public/scripts/universal.js
+++ src/public/scripts/universal.js
@@ -199,9 +199,37 @@ const universal = {
       'name': 'Blue',
       'description': 'A blue theme for Freedeck',
     },
+    green: {
+      'name': 'Green',
+      'description': 'A green theme for Freedeck',
+    },
+    yellow: {
+      'name': 'Yellow',
+      'description': 'A yellow theme for Freedeck',
+    },
+    gruggly: {
+      'name': 'Gruggly',
+      'description': 'A gruggly theme for Freedeck (pantone 448c)',
+    },
+    catppuccin_mocha: {
+      'name': 'Catppuccin Mocha',
+      'description': 'A soothing pastel theme for Freedeck',
+    },
+    dark: {
+      'name': 'Dark',
+      'description': 'A dark theme for Freedeck',
+    },
+    fun: {
+      'name': 'Fun',
+      'description': 'A fun theme for Freedeck',
+    },
     bigger: {
       'name': 'Bigger Buttons',
       'description': 'The default theme with bigger buttons',
+    },
+    circular: {
+      'name': 'Circular',
+      'description': 'A circular theme for Freedeck',
     },
     big_black: {
       'name': 'Bigger Buttons (Black)',

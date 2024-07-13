b7077470f19cef3024b4d100ee1258b4aa74d0e5
ia74 <aidenjs9@icloud.com>
2024-07-12 23:51:44 -0500

    - Color picker now shows color, part of hotfix 1

--- src/public/companion/scripts/main.js
+++ src/public/companion/scripts/main.js
@@ -265,6 +265,7 @@ function editTile(e) {
   document.querySelector('#editor-btn').innerText = e.srcElement.innerText;
   document.querySelector('#editor-btn').style.backgroundImage = 'url("' + JSON.parse(e.srcElement.getAttribute('data-interaction')).data.icon + '")';
   document.querySelector('#editor-btn').style.backgroundColor = JSON.parse(e.srcElement.getAttribute('data-interaction')).data.color;
+  document.querySelector('#color').value = JSON.parse(e.srcElement.getAttribute('data-interaction')).data.color;
   document.querySelector('#name').value = e.srcElement.innerText;
   document.querySelector('#editor-btn').setAttribute('data-pre-edit', e.srcElement.innerText);
   document.querySelector('#editor-btn').setAttribute('data-interaction', e.srcElement.getAttribute('data-interaction'));

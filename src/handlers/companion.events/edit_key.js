3219e60b952eead0442371577858379f04258a83
ia74 <aidenjs9@icloud.com>
2024-07-30 22:27:05 -0500

    - finally, if a tile title changes dynamically we can update it accordingly by UUID

--- src/handlers/companion.events/edit_key.js
+++ src/handlers/companion.events/edit_key.js
@@ -1,22 +1,22 @@
 const config = require('../../managers/settings');
 const eventNames = require('../eventNames');
 
-module.exports = ({io, data}) => {
+module.exports = ({ io, data }) => {
   data = JSON.parse(data);
-  // new name is data.name
-  // old name is data.oldName
+  const { name, interaction } = data;
   const settings = config.settings();
-  let flag = false;
+  const profiles = settings.profiles[settings.profile];
 
-  settings.profiles[settings.profile].forEach((snd) => {
-    if (!(data.oldName in snd)) return;
-    if (snd[data.oldName].uuid !== data.interaction.uuid) return;
-    snd[data.name] = snd[data.oldName];
-    snd[data.name] = data.interaction;
-    if (data.name === data.oldName) return;
-    if (flag) return;
-    delete snd[data.oldName];
-    flag = true;
+  profiles.forEach((snd) => {
+    for (const key in snd) {
+      if (snd[key].uuid === interaction.uuid) {
+        snd[name] = interaction;
+        if (name !== key) {
+          delete snd[key];
+        }
+        break;
+      }
+    }
   });
 
   config.save();

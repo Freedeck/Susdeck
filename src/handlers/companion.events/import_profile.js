7174b6d6328e6ddcd493cf40cc881af9835678fd
ia74 <aidenjs9@icloud.com>
2024-07-31 13:30:35 -0500

    - Make flex wrap row actually wrap - Importing profiles!!

---
+++ src/handlers/companion.events/import_profile.js
@@ -0,0 +1,9 @@
+const config = require('../../managers/settings');
+const eventNames = require('../eventNames');
+
+module.exports = ({io, data}) => {
+  config.settings().profiles[data.name] = data.data;
+  config.settings().profile = data.name;
+  config.save();
+  io.emit(eventNames.default.reload);
+};

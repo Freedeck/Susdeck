495d5b0fc7204c8723cd09401e5edf70f40bcb7f
ia74 <aidenjs9@icloud.com>
2024-08-11 18:08:41 -0500

    - Add new option to center

--- src/handlers/default.events/config_changed.js
+++ src/handlers/default.events/config_changed.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 module.exports = ({io, data}) => {
   if(!fs.existsSync(path.resolve('./src/configs/style.json'))) {
-    fs.writeFileSync(path.resolve('./src/configs/style.json'), JSON.stringify({ scroll: false, fill:false, 'font-size': 15, buttonSize: 6, iconCountPerPage:12, longPressTime: 3, tileRows: 5 }));
+    fs.writeFileSync(path.resolve('./src/configs/style.json'), JSON.stringify({ scroll: false, center:false, fill:false, 'font-size': 15, buttonSize: 6, iconCountPerPage:12, longPressTime: 3, tileRows: 5 }));
   }
   fs.writeFileSync(path.resolve('./src/configs/style.json'), data);
   io.emit(eventNames.default.config_changed, data);

7dcecc60b7a35cea13b31495d418b5759cd57bc9
ia74 <68617740+ia74@users.noreply.github.com>
2025-02-21 00:17:36 -0600

    undo the nested stuff

--- webui/companion/scripts/editor/viewEngine.js
+++ webui/companion/scripts/editor/viewEngine.js
@@ -1,13 +1,6 @@
 import {translatePage} from "../../../shared/localization";
 
 const editorButton = document.querySelector("#editor-btn");
-const view_audioOnly = document.querySelector("#audio-only");
-const view_macroOnly = document.querySelector("#macro-only");
-const view_pluginsOnly = document.querySelector("#plugins-only");
-const view_systemOnly = document.querySelector("#system-only");
-const view_noneOnly = document.querySelector("#none-only");
-const view_profileOnly = document.querySelector("#profile-only");
-
 const selectableViews = [
   "audio",
   "plugins",
